00c39fda09c99b9340e0f686e27c130c5e3440eb
nkleinmann <nekleinmann@gmail.com>
2020-09-15 20:59:43 -0400

    Created burgers db

--- db/schema.sql
+++ db/schema.sql
@@ -1,14 +1,16 @@
-Write SQL queries this file that do the following:
-​
 
-Create the burgers_db.
-Switch to or use the burgers_db.
-Create a burgers table with these fields:
+DROP DATABASE IF EXISTS burgers_db;
 
+CREATE DATABASE burgers_db;
 
-id: an auto incrementing int that serves as the primary key.
+USE burgers_db;
 
-burger_name: a string.
+DROP TABLE IF EXISTS burgers;
 
-devoured: a boolean.
-​
+CREATE TABLE burgers (
+    id int unsigned NOT NULL AUTO_INCREMENT,
+    burger_name varchar(30) NOT NULL,
+    devoured bool NOT NULL,
+    PRIMARY KEY (id)
+)
+
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (id, burger_name, devoured)
+VALUES
+(1, 'turkey burger', false),
+(2, 'delicious burger', true),
+(3, 'salmon burger', false);
