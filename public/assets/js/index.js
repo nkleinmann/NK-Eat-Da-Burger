b81c90bf7027d6cd5655a4ab81d3dab7c859f2e9
nkleinmann <nekleinmann@gmail.com>
2020-09-19 15:20:45 -0400

    debugging devour button and updated comments

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -56,61 +56,25 @@ $(function () {
     burgerDevour.on("click", function () {
         console.log("Devoured!")
         // var id = $(this).data("id");
-        var newDevour = $(this).data("newdevoured");
+        let newDevour = $(this).data("newdevoured");
 
-        var newDevourState = {
-            devour: newDevour
+        let newDevourState = {
+            devour: true
         };
+
+console.log(id, 'IS THE ID');
+console.log(newDevourState, 'IS THE newDevourState');
 
         // Send the PUT request.
         $.ajax("/api/burgers/" + id, {
             type: "PUT",
             data: newDevourState
         }).then(
-            function () {
-                console.log("changed devour to", newDevourState);
+            function (data) {
+                console.log("changed devour to", newDevour);
                 // Reload the page to get the updated list
                 location.reload();
             }
         );
     });
-
-    //   $(".submit").on("submit", function(event) {
-    //     // Make sure to preventDefault on a submit event.
-    //     event.preventDefault();
-
-    //     var newBurger = {
-    //       name: $(".input").val().trim(),
-    //       devoured: $("[name=devour]:checked").val().trim()
-    //     };
-
-    //     // Send the POST request.
-    //     $.ajax("/api/burgers", {
-    //       type: "POST",
-    //       data: newBurger
-    //     }).then(
-    //       function() {
-    //         console.log("created new burger");
-    //         // Reload the page to get the updated list
-    //         location.reload();
-    //       }
-    //     );
-    //   });
-
-    //   $(".deleteBurger").on("click", function(event) {
-    //     var id = $(this).data("id");
-
-    //     // Send the DELETE request.
-    //     $.ajax("/api/burgers/" + id, {
-    //       type: "DELETE"
-    //     }).then(
-    //       function() {
-    //         console.log("deleted burger", id);
-    //         // Reload the page to get the updated list
-    //         location.reload();
-    //       }
-    //     );
-    //   });
-
-
-})
+});
